cd76fdc39b3faebc0b7410e26c236d1ae8757c5b
28emilyma <28emilyma@gmail.com>
2024-02-07 15:19:29 -0500

    added layout (nonfunctional)

--- src/ReadJson.java
+++ src/ReadJson.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,12 +12,29 @@ import java.net.URL;
 // video to load jar
 //https://www.youtube.com/watch?v=QAJ09o3Xl_0
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.swing.*;
+
 // Program for print data in JSON format.
 public class ReadJson {
+
+    private JFrame mainFrame;
+    private JButton next;
+    private JButton previous;
+    //if allies is text area
+    private JTextArea allies;
+    //if allies is label
+    //private JLabel allies;
+    private JLabel characterName;
+    private JPanel buttons;
+    private JPanel charInfo;
+    private int WIDTH = 800;
+    private int HEIGHT = 700;
+
     public static void main(String args[]) throws ParseException {
         // In java JSONObject is used to create JSON object
         // which is a subclass of java.util.HashMap.
@@ -28,8 +48,15 @@ public class ReadJson {
         // To print in JSON format.
         System.out.print(file.get("Tution Fees"));
         pull();
+        ReadJson readjson = new ReadJson();
 
     }
+
+    public ReadJson() {
+        prepareGUI();
+        showEventDemo();
+    }
+
 
     public static void pull() throws ParseException {
         String output = "abc";
@@ -72,24 +99,94 @@ public class ReadJson {
         System.out.println(jsonObjectArray);
 
         try {
+//            for (int i=0; i<jsonObjectArray.size();i++){
+//                JSONObject character = (JSONObject) jsonObjectArray.get(i);
+//                System.out.println(character.get("name"));
+//                JSONArray allies = (JSONArray) character.get("allies");
+//                for (int j = 0; j < character.size(); i++){
+//                    System.out.println(allies.get(j));
+//                }
+//            }
+            JSONObject secretTunnelGuy = (JSONObject) jsonObjectArray.get(0);
+            String name = (String)secretTunnelGuy.get("name");
 
-//            //String name = (String)jsonObjectArray.get("eye_color");
+//            String name = (String)jsonObjectArray.get("name");
 //
-//            //org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObjectArray.get("starships");
+//            org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObjectArray.get("allies");
 //            int n =   msg.size(); //(msg).length();
 //            for (int i = 0; i < n; ++i) {
 //                String test =(String) msg.get(i);
 //                System.out.println(test);
 //                // System.out.println(person.getInt("key"));
 //            }
-//         //   String name= (String)jsonObject.get("height");
+//            //String name= (String)jsonObjectArray.get("height");
+
 //            System.out.println(name);
+//            System.out.println(secretTunnelGuy.get("allies"));
+//            JSONArray allies = (JSONArray)secretTunnelGuy.get("allies");
+//            System.out.println(allies.get(0));
         }
 
         catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+
+    private void prepareGUI(){
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(WIDTH, HEIGHT);
+        mainFrame.setLayout(new BorderLayout());
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        buttons = new JPanel();
+        buttons.setLayout(new GridLayout(2,1));
+
+        charInfo = new JPanel();
+        charInfo.setLayout(new GridLayout(2,1));
+
+        //if allies is label
+//        allies = new JLabel("allies");
+//        allies.setHorizontalAlignment(JLabel.CENTER);
+        //if allies is text area
+        allies = new JTextArea();
+        allies.setBounds(50, 5, WIDTH-100, HEIGHT-50);
+        JScrollPane scrollPane = new JScrollPane(allies);
+        scrollPane.setPreferredSize(new java.awt.Dimension(250, 150));
+        allies.setText("allies");
+
+        characterName = new JLabel("character name");
+        characterName.setHorizontalAlignment(JLabel.CENTER);
+
+        charInfo.add(characterName);
+        charInfo.add(allies);
+        mainFrame.add(charInfo, BorderLayout.CENTER);
+        mainFrame.add(buttons, BorderLayout.SOUTH);
+        mainFrame.setVisible(true);
+    }
+
+    private void showEventDemo() {
+
+        JButton next = new JButton("next character");
+        JButton previous = new JButton("previous character");
+
+
+        buttons.add(previous);
+        buttons.add(next);
+
+
+        //mainFrame.add(topHalf);
+        //mainFrame.add(results);
+        //mainFrame.getContentPane().add(scrollPane);
+
+        //mainFrame.add(button3, BorderLayout.SOUTH);
+        mainFrame.setVisible(true);
+
+//        go.setActionCommand("go");
+//        go.addActionListener(new ButtonClickListener());
 
     }
 }
